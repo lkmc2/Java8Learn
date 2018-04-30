f9d92e683bb3e07516587439f30f3819fe01cb74
lkmc2 <lkmc2@163.com>
2018-04-30 14:08:19 +0800

    添加过滤出偶数列表功能

--- src/Chapter02/ChooseColorAndWeightInApple/ChooseObjWithLambda.java
+++ src/Chapter02/ChooseColorAndWeightInApple/ChooseObjWithLambda.java
@@ -3,6 +3,7 @@ package Chapter02.ChooseColorAndWeightInApple;
 import Chapter01.ChooseColorAndWeightInApple.Apple;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static Chapter01.ChooseColorAndWeightInApple.ListUtils.generateObjToList;
@@ -17,11 +18,13 @@ public class ChooseObjWithLambda {
 
     /**
      * 断言接口
+     *
      * @param <T> 参数类型
      */
     public interface Predicate<T> {
         /**
          * 测试参数是否符合规则
+         *
          * @param t 参数
          * @return 参数是否符合规则
          */
@@ -30,8 +33,9 @@ public class ChooseObjWithLambda {
 
     /**
      * 过滤出符合要求的对象列表
+     *
      * @param list 对象列表
-     * @param p 断言
+     * @param p    断言
      * @return 符合要求的对象列表
      */
     public static <T> List<T> filter(List<T> list, Predicate<T> p) {
@@ -54,5 +58,14 @@ public class ChooseObjWithLambda {
         System.out.println("-----红苹果列表-----");
         // 打印红苹果列表
         printInfoInList(redApples);
+
+
+        // 生成1到10的Integer型列表
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        // 过滤偶数列表
+        List<Integer> evenNumbers = filter(integerList, (Integer i) -> i % 2 == 0);
+        System.out.println("-----偶数列表-----");
+        // 打印偶数列表
+        printInfoInList(evenNumbers);
     }
 }
