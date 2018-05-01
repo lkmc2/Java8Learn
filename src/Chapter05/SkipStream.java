c834e0e92fd5bf08cdaa508e233feff67fb8c3f8
lkmc2 <lkmc2@163.com>
2018-05-01 20:51:34 +0800

    使用skip(n)丢弃Steam产生的前n个元素

--- src/Chapter05/SkipStream.java
+++ src/Chapter05/SkipStream.java
@@ -4,15 +4,16 @@ import Chapter04.Entity.Dish;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
 /**
  * Created by lkmc2 on 2018/5/1.
- * 限制Steam产生的结果个数limit
+ * 使用skip(n)丢弃Steam产生的前n个元素（如果流中元素不足n个，则返回一个空流）
  */
 
-public class LimitStream {
+public class SkipStream {
 
     public static void main(String[] args) {
         // 菜单列表
@@ -28,15 +29,16 @@ public class LimitStream {
                 new Dish("salmon", false, 450, Dish.Type.FISH)
         );
 
-        // 过滤卡路里大于300的菜，并限制只有3条结果
+        // 过滤卡路里高于300的菜，丢弃前两个元素
         List<Dish> dishes = menu.stream()
                 .filter(d -> d.getCalories() > 300)
-                .limit(3)
+                .skip(2)
                 .collect(toList());
         System.out.println(dishes);
+
         /*
         运行结果：
-        [pork, beef, chicken]
+        [chicken, french fries, rice, pizza, salmon]
          */
     }
 
